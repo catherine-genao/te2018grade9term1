0f943b404961793ff2c7a0af3258192bf8ba962e
catherine <cgenao80@gmail.com>
2018-10-13 15:42:16 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,25 @@
 // Example
 let message = "unicurl";
 console.log(message);
+ 
+let doeslikeunicorns = true;
+let doesnotlikenegative = false;
+
+let name = "owllette"
+console.log(name)
+
+let nickname ="olly" 
+let friends name ="ocean blue"
+console.log(friend age)
+
+let catchphase = "uni"
+
+let owlletteage =16;
+console.log(owlletteage)
+
+let oceanage = 17;
+console.log(oceanage)
+
+
 
 // Type your code below!
