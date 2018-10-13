24ea64003293fa08e66a883b6412fd4a69f46350
CodeNext <codenext@Admins-MBP-5.fios-router.home>
2018-10-13 15:41:40 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,3 @@
 // Example
-let message = "Hi How Are You a secret message will be in every 3ed letter hwbsjeddtos djydjoshnojs sjfjsejhslhsalgofhjojhrjs hsihst";
-console.log(message);
-
-// Type your code below!
+let die = undefined;
+console.log(die);
