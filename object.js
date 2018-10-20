9e77b8cd05ce000da04f60faf0e86033cd80f01a
CodeNext <codenext@Admins-MBP-5.fios-router.home>
2018-10-20 15:41:24 -0400

    bobthebob
    bob

---
+++ object.js
@@ -0,0 +1,22 @@
+function character(name)
+
+
+this.name = name;
+this.weapon;
+this. color;
+this.relationship = false; 
+this.getInfo= getCharacter
+}
+
+function getcharacter(){
+
+return this.name + " " + this.weapon + " " + this.color + " " + this. relationship
+}
+
+
+var stone  = NEW character("MIKE");
+stone.color = "PURPLE"
+stone.relationship = false;
+stone.weapon = "fire"
+
+\let person = {isTall: false,Lastname : "Stone", }
